296b9a1c9d76eef77a0b7c3a9fa457c47ecb852b
johnshaughnessy <johnfshaughnessy@gmail.com>
2014-12-15 12:46:43 -0800

    game controller
    committing because comp is restarting every half hour and deleting my
    files

--- object manipulation/Assets/Scripts/GameController.cs
+++ object manipulation/Assets/Scripts/GameController.cs
@@ -2,6 +2,25 @@
 using System.Collections;
 
 public class GameController : MonoBehaviour {
+	public static GameController _singleton;
+	ObjectMovementController objectMoveController;
+	HeadCursor headCursor;
+
+	void Awake(){
+	    if (_singleton == null) {
+			DontDestroyOnLoad(gameObject);
+			_singleton = this;
+		} else if (_singleton != this){
+			Destroy(this);
+		}
+	}
+
+	void Start(){
+	    headCursor = GameObject.Find("OVRPlayerController/OVRCameraRig/CenterEyeAnchor")
+			                   .GetComponent<HeadCursor>();
+		objectMoveController = GetComponent<ObjectMovementController> ();
+	}
+
 
 
 
